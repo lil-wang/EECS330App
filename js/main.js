44e2ce91a581e460ee25468aa84ef741b1143478
ginowangsh4 <sihengwang2019@u.northwestern.edu>
2017-02-09 02:07:32 -0600

    Updated index page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-$(document).ready(function(){       
+$(document).ready(function(){
    var scroll_start = 0;
    var carousel = $('#myCarousel');
    var offset = carousel.offset();
     if (carousel.length){
-   $(document).scroll(function() { 
+   $(document).scroll(function() {
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
           $('.navbar').css('background', '#fff');
+          $('.navbar').fadeTo(500,0.9);
        } else {
-          $('.navbar').css('background', 'transparent');
+          $('.navbar').css('background', 'transparent');''
        }
    });
     }
