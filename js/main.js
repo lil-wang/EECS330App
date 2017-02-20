0ebf446300e3f006dd4a0e351151309f47ff49b2
ginowangsh4 <sihengwang2019@u.northwestern.edu>
2017-02-20 13:50:15 -0600

    navbar test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+   $('.log-in').css('color', '#FFFFFF');
    var scroll_start = 0;
    var carousel = $('#myCarousel');
    var offset = carousel.offset();
@@ -8,11 +9,12 @@ $(document).ready(function(){
       if(scroll_start > offset.top) {
           $('.navbar').css('background', '#00a5f6');
           $('.navbar').fadeTo(500,0.9);
-          $('.log-in').css('color', '#FFFFFF');
        } else {
           $('.navbar').css('background', 'transparent');
-          $('.log-in').css('color', '#00a5f6');
        }
    });
     }
+
+
+    
 });
