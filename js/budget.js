8422bd4d52b5e7c078269f5833c990dc2b0595e0
ginowangsh4 <sihengwang2019@u.northwestern.edu>
2017-02-25 21:53:00 -0600

    confirm box for edit budget and add transaction

--- js/budget.js
+++ js/budget.js
@@ -1,11 +1,18 @@
-var display = document.getElementById("categdropdown");
+$(document).ready(function(){
+var display = document.getElementById("budget-category");
 
 for (var i=1; i<=11; i++) {
 	var button = document.getElementById("choice" + i);
-	if (button != null) {  	 	
+	if (button != null) {
 		button.onclick = function(e) {
 			display.innerHTML = e.target.innerHTML;
 		}
-	}			   
+	}
 }
+})
 
+function update_budget(){
+	var category = document.getElementById("budget-category").innerHTML;
+	var amount = document.getElementById("budget-amount").value;
+	confirm("Success! You have updated budget category of \"" + category + "\" to $" + amount + "!");
+}
