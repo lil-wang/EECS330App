d5c77fda77d9941140c0d46552d67c719d890913
ginowangsh4 <sihengwang2019@u.northwestern.edu>
2017-02-27 12:51:27 -0600

    implementation of setting page pop-up

--- js/setting.js
+++ js/setting.js
@@ -1,1 +1,18 @@
+var account_number = document.getElementById("account-number");
+var account_type = document.getElementById("account-type");
+var account_name = document.getElementById("account-name");
 
+for (var i=1; i<=5; i++) {
+    var acc = document.getElementById("choice" + i);
+    if (acc != null) {
+        acc.onclick = function(e) {
+            account_type.innerHTML = e.target.innerHTML;
+        }
+    }
+}
+
+function add_account(){
+  confirm("Success! You have added a new bank account from " + account_name.value + ": \n" +
+  "- Account type: " + account_type.innerHTML + "\n- Account Number: " + account_number.value);
+
+};
